53c9d01c84a6f181161e75f55cfb231353460178
Dear <kunsaya.j@kkumail.com>
2026-01-05 17:08:44 +0700

    Lab04

---
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -0,0 +1,64 @@
+<template>
+  <ion-page>
+    <!-- Header -->
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+          <ion-card-subtitle>
+            โดย นางสาวกุลศยา จันภูงา รหัส 663380145-7
+          </ion-card-subtitle>
+      </ion-toolbar>
+    </ion-header>
+  <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+
+<!-- Grid แสดงรูป -->
+      <ion-grid>
+        <ion-row>
+          <ion-col v-for="photo in photos" :key="photo.filepath" size="6">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <!-- Floating Action Button -->
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="addNewToGallery()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { camera } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+
+const { photos, addNewToGallery } = usePhotoGallery();
+</script>
